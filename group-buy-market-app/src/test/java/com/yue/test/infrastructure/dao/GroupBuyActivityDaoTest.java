85eb2737f67c017cb984c038d30528c83f6db42a
fhhbjb <161243157+fhhbjb@users.noreply.github.com>
2025-09-27 19:55:37 +0800

    feat: 第一节测试

---
+++ group-buy-market-app/src/test/java/com/yue/test/infrastructure/dao/GroupBuyActivityDaoTest.java
@@ -0,0 +1,27 @@
+package com.yue.test.infrastructure.dao;
+
+import com.alibaba.fastjson.JSON;
+import com.yue.infrastructure.dao.IGroupBuyActivityDao;
+import com.yue.infrastructure.dao.po.GroupBuyActivity;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Slf4j
+@SpringBootTest
+public class GroupBuyActivityDaoTest {
+
+    @Resource
+    private IGroupBuyActivityDao groupBuyActivityDao;
+
+    @Test
+    public void test_queryGroupBuyActivityList() {
+        List<GroupBuyActivity> groupBuyActivities = groupBuyActivityDao.queryGroupBuyActivityList();
+        log.info("测试结果:{}", JSON.toJSONString(groupBuyActivities));
+    }
+}
---
+++ group-buy-market-infrastructure/src/main/java/com/yue/infrastructure/dao/IGroupBuyActivityDao.java
@@ -0,0 +1,18 @@
+package com.yue.infrastructure.dao;
+
+import com.yue.infrastructure.dao.po.GroupBuyActivity;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 拼团活动Dao
+ * @create 2024-12-07 10:10
+ */
+@Mapper
+public interface IGroupBuyActivityDao {
+
+    List<GroupBuyActivity> queryGroupBuyActivityList();
+
+}
