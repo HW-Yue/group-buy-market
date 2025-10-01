134c42451a59c18dd5f85a16b4490417d8da8d68
fhhbjb <161243157+fhhbjb@users.noreply.github.com>
2025-10-01 20:48:06 +0800

    feat: 人群标签数据采集

---
+++ group-buy-market-infrastructure/src/main/java/com/yue/infrastructure/adapter/repository/TagRepository.java
@@ -0,0 +1,79 @@
+package com.yue.infrastructure.adapter.repository;
+
+import com.yue.domain.tag.adapter.repository.ITagRepository;
+import com.yue.domain.tag.model.entity.CrowdTagsJobEntity;
+import com.yue.infrastructure.dao.ICrowdTagsDao;
+import com.yue.infrastructure.dao.ICrowdTagsDetailDao;
+import com.yue.infrastructure.dao.ICrowdTagsJobDao;
+import com.yue.infrastructure.dao.po.CrowdTags;
+import com.yue.infrastructure.dao.po.CrowdTagsDetail;
+import com.yue.infrastructure.dao.po.CrowdTagsJob;
+import com.yue.infrastructure.redis.IRedisService;
+import org.redisson.api.RBitSet;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 人群标签仓储
+ * @create 2024-12-28 13:12
+ */
+@Repository
+public class TagRepository implements ITagRepository {
+
+    @Resource
+    private ICrowdTagsDao crowdTagsDao;
+    @Resource
+    private ICrowdTagsDetailDao crowdTagsDetailDao;
+    @Resource
+    private ICrowdTagsJobDao crowdTagsJobDao;
+
+    @Resource
+    private IRedisService redisService;
+
+    @Override
+    public CrowdTagsJobEntity queryCrowdTagsJobEntity(String tagId, String batchId) {
+        CrowdTagsJob crowdTagsJobReq = new CrowdTagsJob();
+        crowdTagsJobReq.setTagId(tagId);
+        crowdTagsJobReq.setBatchId(batchId);
+
+        CrowdTagsJob crowdTagsJobRes = crowdTagsJobDao.queryCrowdTagsJob(crowdTagsJobReq);
+        if (null == crowdTagsJobRes) return null;
+
+        return CrowdTagsJobEntity.builder()
+                .tagType(crowdTagsJobRes.getTagType())
+                .tagRule(crowdTagsJobRes.getTagRule())
+                .statStartTime(crowdTagsJobRes.getStatStartTime())
+                .statEndTime(crowdTagsJobRes.getStatEndTime())
+                .build();
+    }
+
+    @Override
+    public void addCrowdTagsUserId(String tagId, String userId) {
+        CrowdTagsDetail crowdTagsDetailReq = new CrowdTagsDetail();
+        crowdTagsDetailReq.setTagId(tagId);
+        crowdTagsDetailReq.setUserId(userId);
+
+        try {
+            crowdTagsDetailDao.addCrowdTagsUserId(crowdTagsDetailReq);
+            //注意这里,如果有重复数据,就会catch异常,下面的代码就不会执行
+            // 获取BitSet
+            RBitSet bitSet = redisService.getBitSet(tagId);
+            bitSet.set(redisService.getIndexFromUserId(userId), true);
+        } catch (DuplicateKeyException ignore) {
+            // 忽略唯一索引冲突
+        }
+    }
+
+    @Override
+    public void updateCrowdTagsStatistics(String tagId, int count) {
+        CrowdTags crowdTagsReq = new CrowdTags();
+        crowdTagsReq.setTagId(tagId);
+        crowdTagsReq.setStatistics(count);
+
+        crowdTagsDao.updateCrowdTagsStatistics(crowdTagsReq);
+    }
+
+}
